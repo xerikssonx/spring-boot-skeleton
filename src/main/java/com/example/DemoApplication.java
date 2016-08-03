42919037de1aa9bd01b528f7ccf4e7f667b235d3
edhar_vakhitov <edhar.vakhitov@cloudtp.com>
2016-08-03 14:15:55 +0300

    add Spring data rest

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,9 +1,12 @@
-package com.example.configuration;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/dao/UserDao.java
+++ src/main/java/com/example/dao/UserDao.java
@@ -1,9 +1,12 @@
 package com.example.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.entity.User;
 
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserDao  extends CrudRepository<User, Long>{
 
+	
 }
