0a87f41371f45fb41baaf0dba8b23b9680d55a2c
edhar_vakhitov <edhar.vakhitov@cloudtp.com>
2016-08-05 09:31:43 +0300

    Add @Parm for documentation

--- src/main/java/com/example/dao/UserDao.java
+++ src/main/java/com/example/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.example.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.entity.User;
@@ -8,6 +9,6 @@ import com.example.entity.User;
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserDao extends CrudRepository<User, Long> {
 
-	User findByUsername(String username);
+	User findByUsername(@Param(value = "username") String username);
 	
 }
